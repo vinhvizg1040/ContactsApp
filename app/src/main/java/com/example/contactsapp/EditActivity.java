c08e0e4009ede893310ce742895d64b6220ee763
Thái Vĩnh <vinhvizg1040@gmail.com>
2022-06-04 16:56:48 +0700

    import avatar image services - storage

--- app/src/main/java/com/example/contactsapp/EditActivity.java
+++ app/src/main/java/com/example/contactsapp/EditActivity.java
@@ -1,22 +1,35 @@
 package com.example.contactsapp;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.contactsapp.controller.ContactDAO;
 import com.example.contactsapp.entities.Contact;
+import com.example.contactsapp.services.ImageServices;
+import com.example.contactsapp.services.ValidateServices;
+
+import java.io.File;
+import java.io.InputStream;
 
 
 public class EditActivity extends AppCompatActivity {
 
     EditText txteName, txtePhone, txteMail;
     Button btnDelete, btnSave, btnCall;
+    ImageView imgEditAvatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,9 @@ public class EditActivity extends AppCompatActivity {
         txteName = findViewById(R.id.txteName);
         txtePhone = findViewById(R.id.txtePhone);
         txteMail = findViewById(R.id.txteMail);
+
+        imgEditAvatar = findViewById(R.id.imgEditAvatar);
+        imgEditAvatar.setOnClickListener(this::openFileChooser);
 
         //Delete
         btnDelete = findViewById(R.id.btnDelete);
@@ -43,8 +59,6 @@ public class EditActivity extends AppCompatActivity {
 
 
         showInfo();
-
-
     }
 
     public void showInfo() {
@@ -54,6 +68,16 @@ public class EditActivity extends AppCompatActivity {
         txteName.setText(contact.getName());
         txteMail.setText(contact.getEmail());
         txtePhone.setText(contact.getPhone());
+
+        try {
+            Uri imageUri = Uri.fromFile(new File(contact.getImgPath()));
+            InputStream imageStream = getContentResolver().openInputStream(imageUri);
+            Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+            imgEditAvatar.setImageBitmap(bitmap);
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.d("e",e.getMessage());
+        }
     }
 
     public void deleteButton() {
@@ -71,6 +95,10 @@ public class EditActivity extends AppCompatActivity {
             ContactDAO dao = new ContactDAO(getApplicationContext());
             dao.removeContact(contact.getId());
 
+            //delete image
+            ImageServices imageServices = new ImageServices();
+            imageServices.deleteImage(contact.getImgPath());
+
             Intent intent = new Intent(EditActivity.this, MainActivity.class);
             startActivity(intent);
 
@@ -83,7 +111,8 @@ public class EditActivity extends AppCompatActivity {
     }
 
     public void saveButton() {
-        if (validateInfo(txteName.getText().toString(), txtePhone.getText().toString(), txteMail.getText().toString())){
+        ValidateServices validateServices = new ValidateServices();
+        if (validateServices.validateInfo(txteName, txtePhone, txteMail)){
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Confirm");
             builder.setMessage("Are you sure?");
@@ -100,7 +129,14 @@ public class EditActivity extends AppCompatActivity {
                 contact.setPhone(txtePhone.getText().toString());
                 contact.setEmail(txteMail.getText().toString());
 
+                //delete image
+                ImageServices imageServices = new ImageServices();
+                imageServices.deleteImage(contact.getImgPath());
+                //save image
+                contact.setImgPath(imageServices.saveImage(imgEditAvatar, getApplicationContext()));
+
                 dao.editContact(contact);
+
                 Intent intent = new Intent(EditActivity.this, MainActivity.class);
                 startActivity(intent);
 
@@ -118,26 +154,32 @@ public class EditActivity extends AppCompatActivity {
         startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
     }
 
-    //Regex
-    public Boolean validateInfo(String name, String phone, String mail){
-        final String phoneRegex = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
-                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
-                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
-        final String mailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$";
+    int requestcode = 1;
 
-        if (name.trim().length() == 0){
-            txteName.requestFocus();
-            txteName.setError("Field cannot empty");
-            return false;
-        }else if (!phone.matches(phoneRegex)){
-            txtePhone.requestFocus();
-            txtePhone.setError("wrong format");
-            return false;
-        }else if (!mail.trim().matches(mailRegex)){
-            txteMail.requestFocus();
-            txteMail.setError("email is not valid");
-            return false;
-        }
-        return true;
+    public void openFileChooser(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, requestcode);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == requestcode && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                return;
+            }
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                imgEditAvatar.setImageBitmap(selectedImage);
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
 }
